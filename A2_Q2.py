11cc76d4a716400fdc71c149507dcc538752df6e
mhameeducalgary <mohammad.hameed@ucalgary.ca>
2020-03-26 20:29:45 -0600

    Added A2_Q2.py

--- A2_Q2.py
+++ A2_Q2.py
@@ -2,51 +2,32 @@ from pprint import pprint
 import numpy
 import scipy
 import scipy.linalg
+import timeit
 
 A = numpy.array([[7, 3, -1, 2], [3, 8, 1, -4], [-1, 1, 4, -1], [2, -4, -1, 6]])
+n3 = A.shape[0] ** 3.0
+lu_ops = (2.0/3.0) * n3
+chol_ops = (1.0/3.0) * n3
+qr_ops = (2 * n3) - ((2.0/3.0) * n3)
 
 # LU Factorization
-print ("LU Factorization:")
-P, L, U = scipy.linalg.lu(A)
+def lu_fact():
+    P, L, U = scipy.linalg.lu(A)
 
-print ("A:")
-pprint(A)
-
-print ("P:")
-pprint(P)
-
-print ("L:")
-pprint(L)
-
-print ("U:")
-pprint(U)
-
-print ()
-
-
-# Cholesky Factorization
-print ("Cholesky Factorization:")
-L = numpy.linalg.cholesky(A)
-
-print ("A:")
-pprint(A)
-
-print ("L:")
-pprint(L)
-
-print ()
+# # Cholesky Factorization
+def chol_fact():
+    L = numpy.linalg.cholesky(A)
 
 # QR Factorization
-print ("QR Factorization:")
-Q, R = numpy.linalg.qr(A)
+def qr_fact():
+    Q, R = numpy.linalg.qr(A)
 
-print ("A:")
-pprint(A)
+if __name__ == '__main__':
+    lu_t = timeit.timeit("lu_fact()", setup="from __main__ import lu_fact", number=1)
+    print("LU FLOPS: " + str(lu_ops/lu_t))
 
-print ("Q:")
-pprint(Q)
+    chol_t = timeit.timeit("chol_fact()", setup="from __main__ import chol_fact", number=1)
+    print("CH FLOPS: " + str(chol_ops/chol_t))
 
-print ("R:")
-pprint(R)
-
-print ()
+    qr_t = timeit.timeit("qr_fact()", setup="from __main__ import qr_fact", number=1)
+    print("QR FLOPS: " + str(qr_ops/qr_t))
